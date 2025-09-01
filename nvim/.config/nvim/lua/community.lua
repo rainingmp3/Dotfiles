74d8a7fd06d20c1e1988c6ce27b2043493e769a6
rainingmp3 <dotagenii@gmail.com>
2025-09-01 19:01:50 +0200

    Astrovim is much better

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,19 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.markdown" },
+
+  -- import/override with your plugins folder
+}
